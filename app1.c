fc178a0967a8d439a2773f4415d0401a36a01e2b
korpe <korpe@cs.bilkent.edu.tr>
2024-04-06 16:44:39 +0300

    Update app1.c

--- app1.c
+++ app1.c
@@ -6,9 +6,11 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <semaphore.h>
+#include <sys/mman.h>
+#include <time.h>
 #include "mf.h"
 
-#define COUNT 5
+#define COUNT 10
 char *semname1 = "/semaphore1";
 char *semname2 = "/semaphore2";
 sem_t *sem1, *sem2;
@@ -18,45 +20,78 @@ int
 main(int argc, char **argv)
 {
     int ret,  i, qid;
+    char sendbuffer[MAX_DATALEN];
+    int n_sent, n_received;
+    char recvbuffer[MAX_DATALEN];
+    int sentcount;
+    int receivedcount;
+    int totalcount;
+    
+    totalcount = COUNT;
+    if (argc == 2)
+        totalcount = atoi(argv[1]);
 
     sem1 = sem_open(semname1, O_CREAT, 0666, 0); // init sem
     sem2 = sem_open(semname2, O_CREAT, 0666, 0); // init sem
 
+    srand(time(0));
+    printf ("RAND_MAX is %d\n", RAND_MAX);
+    
     ret = fork();
     if (ret > 0) {
         // parent process - P1
-        char *bufptr = (char *) malloc (MAX_DATALEN);
-        sem1 = sem_open(semname1, 0);
-        sem2 = sem_open(semname2, 0);
+        // parent will create a message queue
+       // sem1 = sem_open(semname1, 0);
+        //sem2 = sem_open(semname2, 0);
+        
         mf_connect();
+        
         mf_create (mqname1, 16); //  create mq;  16 KB
+        
         qid = mf_open(mqname1);
+        
         sem_post (sem1);
         
-        for (i = 0; i < COUNT; ++i) {
-            sprintf (bufptr, "%s-%d", "MessageData", i);
-            mf_send (qid, (void *) bufptr, strlen(bufptr)+1);
+        while (1) {
+            n_sent = rand() % MAX_DATALEN;
+            //sprintf (databuf, "%s-%d", "MessageData", i);
+            ret = -1;
+            while (ret == -1)
+                ret = mf_send (qid, (void *) sendbuffer, n_sent);
+            printf ("app sent message, datalen=%d\n", n_sent);
+            sentcount++;
+            if (sentcount == totalcount)
+                break;
         }
         mf_close(qid);
-        mf_disconnect();
         sem_wait(sem2);
         mf_remove(mqname1);   // remove mq
+        mf_disconnect();
     }
     else if (ret == 0) {
         // child process - P2
-        char *bufptr = (char *) malloc (MAX_DATALEN);
-        sem1 = sem_open(semname1, 0);
-        sem2 = sem_open(semname2, 0);
+        // child will connect to open and use the queue
+        //sem1 = sem_open(semname1, 0);
+        //sem2 = sem_open(semname2, 0);
         sem_wait (sem1);
+        
         mf_connect();
+        
         qid = mf_open(mqname1);
-        for (i = 0; i < COUNT; ++i) {
-            mf_recv (qid, (void *) bufptr, MAX_DATALEN);
-            printf ("%s\n", bufptr);
+        
+        while (1) {
+            n_received = -1;
+            while (n_received == -1) {
+                n_received =  mf_recv (qid, (void *) recvbuffer, MAX_DATALEN);
+            }
+            printf ("app received message, datalen=%d\n", n_received);
+            receivedcount++;
+            if (receivedcount == totalcount)
+                break;
         }
         mf_close(qid);
         mf_disconnect();
         sem_post(sem2);
     }
-    return 0;
+	return 0;
 }
