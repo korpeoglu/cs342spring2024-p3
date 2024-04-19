3e80e26a95c837a89bd95091b99a77bda18527a7
korpe <korpe@cs.bilkent.edu.tr>
2024-04-19 10:17:54 +0300

    Update consumer.c

--- consumer.c
+++ consumer.c
@@ -1,5 +1,4 @@
 //// run consumer first, then producer.
-
 #include <assert.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -22,6 +21,8 @@ main(int argc, char **argv)
     int qid;
     char recvbuffer[MAX_DATALEN];
     int i = 0;
+    int n_recv;
+    int j;
 
     if (argc != 1) {
         printf ("usage: ./consumer\n");
@@ -32,11 +33,20 @@ main(int argc, char **argv)
     mf_create (mqname, 16);
     qid = mf_open(mqname);
     while (1) {
-        mf_recv(qid, (void *) recvbuffer, MAX_DATALEN);
+        n_recv = mf_recv(qid, (void *) recvbuffer, MAX_DATALEN);
         if (recvbuffer[0] == -1)
             break;
+        // check if data received correctly
+        for (j = 1; j < n_recv; ++j) {
+            if (recvbuffer[j] != 'A') {
+                printf ("data corruption\n");
+                exit (1);
+            }
+        }
+        printf ("data integrity check: success; size of message=%d\n", n_recv);
+
         i++;
-        printf ("received data message %d\n", i);
+        printf ("received data message %d, size=%d\n", i, n_recv);
     }
     mf_close(qid);
     mf_remove(mqname); 
